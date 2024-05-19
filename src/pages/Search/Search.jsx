6333cb236817b6e49e8cabc29641f318898937a9
4kyoune <wangyyputra@gmail.com>
2024-05-19 20:47:09 +0700

    Update Search.jsx

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,50 +1,42 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { getSearchResult } from "../../Hooks/Api/index.js";
-import Card from "../../components/elements/Card/Card.jsx";
-import Title from "../../components/elements/Title/Title.jsx";
-import Loading from "../../components/layouts/Loading/Loading.jsx";
-import { Helmet } from "react-helmet";
+import React from "react";
+import { useSearchParams } from "react-router-dom";
+import { getAnimeSearch } from "../api/jikan";
+import AnimeCollection from "../components/MainContainer/AnimeCollection";
+import Genre from "../components/Genre/Genre";
+import TopTenAnime from "../components/TopTen/TopTenAnime";
+import LoadingSpinner from "../components/LoadingSpinner";
+import Error from "../components/AnimeNotFound/Error";
+import { motion } from "framer-motion";
+export default function SearchResults() {
+  const [searchParams] = useSearchParams();
+  const animeData = getAnimeSearch(
+    searchParams.get("name"),
+    searchParams.get("parameter")
+  );
 
-const Search = () => {
-    const {value} = useParams();
-
-    const [searchResult, setSearchResult] = useState([]);
-
-    useEffect(() => {
-        const searchingResult = async () => {
-            const result = await getSearchResult(value);
-            setSearchResult(result);
-        }
-
-        searchingResult();
-    }, [value])
-
-    return(
-        <>
-            { searchResult.status === "success" ?
-                (<main className="px-4 md:px-16 my-5 md:my-14">
-                    <Title>Hasil pencarian "{value}"</Title>
-                    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5 py-5 px-6">
-                    {
-                        searchResult.search_results.map((data, index) => {
-                            return(
-                                <Card
-                                    key={index}
-                                    imgUrl={data.thumb}
-                                    href={`/anime/${data.id}`}
-                                    title={data.title}
-                                    episode={data.status}
-                                />
-                            )
-                        })
-                    }
-                    </div>
-                </main>) : <Loading />
-
-            }
-        </>
-    )
+  return (
+    <motion.div
+      className=" main-container d-flex  "
+      style={
+        window.innerWidth < 1081 ? { flexDirection: "column-reverse" } : {}
+      }
+      initial={{ opacity: 0 }}
+      animate={{ x: [window.innerWidth / 2, 0], opacity: 1 }}
+      transition={{ duration: 0.5, ease: "easeOut" }}
+    >
+      <div className="sidebar-wrapper d-flex-fd-column">
+        <Genre />
+        <TopTenAnime />
+      </div>
+      <div className="collections-wrapper">
+        {animeData.isLoading ? (
+          <LoadingSpinner />
+        ) : animeData.data?.data.length < 1 ? (
+          <Error />
+        ) : (
+          <AnimeCollection collectionName="Search Results" data={animeData} />
+        )}
+      </div>
+    </motion.div>
+  );
 }
-
-export default Search;
