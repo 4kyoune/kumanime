3e715cee867d3683d553a74aea6f5af4aa8f4002
4kyoune <wangyyputra@gmail.com>
2024-05-19 20:40:30 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,46 @@
-import { Outlet } from "react-router-dom"
-import Navbar from "./components/layouts/Navbar/Navbar"
-import Homepage from "./pages/Home/Homepage"
-import Footer from "./components/layouts/Footer/Footer"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { lazy } from "react";
+import Nav from "./Layouts/Nav";
+const Home = lazy(() => import("./pages/Home"));
+const Genre = lazy(() => import("./pages/Genre"));
 
+const GenreSidebar = lazy(() => import("./Layouts/GenreSidebar"));
 
-function App() {
+const AnimeInfoRandom = lazy(() =>
+  import("./components/AnimeInfo/AnimeInfoRandom")
+);
+const AnimeByFilter = lazy(() => import("./pages/AnimeByFilter"));
+const AnimeByType = lazy(() => import("./pages/AnimeByType"));
+const WatchAnime = lazy(() => import("./pages/WatchAnime/WatchAnime"));
+const SearchResults = lazy(() => import("./pages/SearchResults"));
+const RecommendedTopTen = lazy(() => import("./Layouts/RecommendedTopTen"));
+const AnimeInfoJikan = lazy(() =>
+  import("./components/AnimeInfo/AnimeInfoJikan")
+);
+const AnimeInfoKitsu = lazy(() =>
+  import("./components/AnimeInfo/AnimeInfoKitsu")
+);
 
+export default function App() {
   return (
-    <>
-      <Navbar />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Nav />}>
+          <Route index element={<Home />} />
+          <Route path="details" element={<RecommendedTopTen />}>
+            <Route path="kitsu/:id" element={<AnimeInfoKitsu />} />
+            <Route path="jikan/:id" element={<AnimeInfoJikan />} />
+            <Route path="random" element={<AnimeInfoRandom />} />
+          </Route>
+          <Route path="grid" element={<GenreSidebar />}>
+            <Route path="genre" element={<Genre />} />
+            <Route path="filter" element={<AnimeByFilter />} />
+            <Route path="type" element={<AnimeByType />} />
+          </Route>
+          <Route path="search" element={<SearchResults />} />
+          <Route path="watch" element={<WatchAnime />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
